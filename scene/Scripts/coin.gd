d63ed4db6429ae3894b683a57c3576ba724c1b30
Tanmay Shet <tanmayshet2010@gmail.com>
2026-02-22 14:13:25 +0530

    Added coin pickup sound

--- scene/Scripts/coin.gd
+++ scene/Scripts/coin.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 @onready var game_maneger: Node = %GameManeger
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	game_maneger.add_point()
-	queue_free()
+	animation_player.play("pickup")
